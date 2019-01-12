4679f057c32f5b4f129f7d5eb466a8bdd2331647
0xJeremy <jeremy.kanovsky@tufts.edu>
2019-01-11 22:52:29 -0500

    Added Form Interaction

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var clear_form = document.getElementById("clear_form");
+var calculate_price = document.getElementById("calculate_price");
+
+clear_form.onclick = function() {
+	document.getElementById("beds").value = "";
+	document.getElementById("baths").value = "";
+	document.getElementById("sqfootage").value = "";
+	document.getElementById("zipcode").value = "";
+	document.getElementById("avgprice").value = "";
+}
+
+calculate_price.onclick = function() {
+	num_beds = document.getElementById("beds").value;
+	num_baths = document.getElementById("baths").value;
+	square_footage = document.getElementById("sqfootage").value;
+	zipcode = document.getElementById("zipcode").value;
+	avg_price = document.getElementById("avgprice").value;
+}
