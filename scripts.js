b5922149814bff55653257e92525809579f88db3
0xJeremy <jeremy.kanovsky@tufts.edu>
2019-01-12 15:51:14 -0500

    Bug Fixes

--- scripts.js
+++ scripts.js
@@ -7,6 +7,10 @@ window.onload = async function load_model() {
 	console.log("Model Loaded");
 }
 
+function add_commas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 clear_form.onclick = function() {
 	document.getElementById("beds").value = "";
 	document.getElementById("baths").value = "";
@@ -24,18 +28,18 @@ calculate_price.onclick = function() {
 	avg_price = parseFloat(document.getElementById("avgprice").value);
 	if(num_beds && num_baths && square_footage && zipcode && avg_price) {
 		price = predict_price();
-		document.getElementById("result").innerHTML = "</br></br><div class=\"text-center alert alert-success\" role=\"alert\">Calculated Price: $" + price + "</div>";	
+		document.getElementById("result").innerHTML = "</br></br><div class=\"text-center alert alert-success\" role=\"alert\">Calculated Price: $" + add_commas(price) + "</div>";	
 	}
 	else {
 		document.getElementById("result").innerHTML = "</br></br><div class=\"text-center alert alert-danger\" role=\"alert\">Error: Missing Information</div>";
 	}
 }
 
-async function predict_price() {
-	prediction_tensor = tf.tensor2d([[num_beds, num_baths, square_footage, zipcode]], [1, 4]);
+function predict_price() {
+	prediction_tensor = tf.tensor2d([num_beds, num_baths, square_footage, zipcode], [1, 4]);
 	const prediction = model.predict(prediction_tensor);
-	console.log(prediction.toString());
-	return prediction;
+	value = prediction.dataSync();
+	return parseInt(value[0], 10);
 }
 
 // https://js.tensorflow.org/tutorials/import-saved-model.html
